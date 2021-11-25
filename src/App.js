9e902f88140e17ec134ec1c201aeae400592a992
Nikolaevmar <marioniko001@gmail.com>
2021-11-25 12:41:19 +0200

    added a starter navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
