a21195c6cbf4803889f5ac093c90323a4bff0fe7
Nikolaevmar <marioniko001@gmail.com>
2021-11-25 13:03:13 +0200

    added the sign up form

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,10 +1,44 @@
+import { useState } from "react";
 import styles from "./Register.module.css";
 
-export default function Register() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  const [name, setName] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+      </label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <label>
+        <span>Username:</span>
+      </label>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <label>
+        <span>Password:</span>
+      </label>
+      <input
+        type="password"
+        onChange={(e) => setPass(e.target.value)}
+        value={password}
+      />
+      <button className="btn">Sign up</button>
+    </form>
+  );
+}
