6f7556ed3a15e1a7dbc533919c7cc19d0e53cc91
Nikolaevmar <marioniko001@gmail.com>
2021-11-25 13:48:58 +0200

    set up firebase and changed some styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
             <ul>
                 <li className={styles.title}>Budget tracker</li>
                 <li><Link to='/login'>Login</Link></li>
-                <li><Link to='register'>Sign up</Link></li>
+                <li><Link to='/register'>Sign up</Link></li>
             </ul>
         </nav>
     )
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtZSHak0--EkM2hEoFd0io1sAtoN36Kfc",
+  authDomain: "mybudget-tracker.firebaseapp.com",
+  projectId: "mybudget-tracker",
+  storageBucket: "mybudget-tracker.appspot.com",
+  messagingSenderId: "358610538171",
+  appId: "1:358610538171:web:656cca44521189e180639e",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const firestore = firebase.firestore();
+const fireauth = firebase.auth();
+
+export { firestore, fireauth };
