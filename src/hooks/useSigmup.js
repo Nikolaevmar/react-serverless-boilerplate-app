22bb85cee1d12e59bcbe807ed2c06e85e2efa95b
Nikolaevmar <marioniko001@gmail.com>
2021-11-26 12:28:07 +0200

    added the signup hook

---
+++ src/hooks/useSigmup.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { fireauth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, name) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //signup user
+      const response = await fireauth.createUserWithEmailAndPassword(email,password);
+      if (!response) {
+        throw new Error("Could not sign up");
+      }
+      //add display name for user
+      await response.user.updateProfile({ name });
+
+      setIsPending(false);
+      setError(null);
+
+    } catch (err) {
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+};
