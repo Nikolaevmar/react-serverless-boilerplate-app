dffec8891401d1b097e58a7bc1a1902c2dff5c69
Nikolaevmar <marioniko001@gmail.com>
2021-11-26 13:43:42 +0200

    wrapped the app with context and the dispatch for loggin in

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { fireauth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, name) => {
     setError(null);
@@ -11,16 +13,21 @@ export const useSignup = () => {
 
     try {
       //signup user
-      const response = await fireauth.createUserWithEmailAndPassword(email, password);
+      const response = await fireauth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
       if (!response) {
         throw new Error("Could not sign up");
       }
       //add display name for user
       await response.user.updateProfile({ name });
 
+      //dispatch login action
+      dispatch({ type: "LOG_IN", payload: response.user });
+
       setIsPending(false);
       setError(null);
-
     } catch (err) {
       setError(err.message);
       setIsPending(false);
