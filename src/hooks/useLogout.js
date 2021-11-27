7b972ab938ee02ad46f4d5250fd8f05a0e072d5b
Nikolaevmar <marioniko001@gmail.com>
2021-11-27 12:58:43 +0200

    added clean up functions to logout and signup hooks

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { fireauth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
@@ -14,13 +15,23 @@ export const useLogout = () => {
     try {
       await fireauth.signOut();
       dispatch({ type: "LOG_OUT" });
-      setIsPending(false);
-      setError(null);
+
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  //Clean up function that prevents state updating when a component is unmounted.
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
   return { logout, error, isPending };
 };
