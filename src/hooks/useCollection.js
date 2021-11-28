465b4e74b474c6f50c2b191859c32c092fb2c4ec
Nikolaevmar <marioniko001@gmail.com>
2021-11-28 18:56:37 +0200

    added firestore queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import { firestore } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
+  //if we don't use a ref we will get an infinite loop in useEffect
+ //_query is an array and is 'different' on every function call which triggers the infinite loop
+  const query = useRef(_query).current;
+
   useEffect(() => {
     let ref = firestore.collection(collection);
+
+    if(query){
+      ref = ref.where(...query)
+    }
 
     //the snapshot represents the collection at that moment in time & will contain the docs on it
     const unsub = ref.onSnapshot(
@@ -24,6 +32,6 @@ export const useCollection = (collection) => {
     );
     //unsub on unmount
     return () => unsub();
-  }, [collection]);
+  }, [collection, query]);
   return { documents, error };
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,22 @@
-import styles from './Home.module.css'
-import TransactionForm from './TransactionForm'
-import {useAuthContext} from '../../hooks/useAuthContext'
+import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 export default function Home() {
-    const {user} = useAuthContext();
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                Transaction
-            </div>
-            <div className={styles.sidebar}>
-                <TransactionForm uid={user.uid}/>
-            </div>
-        </div>
-    )
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions", ['uid', '==', user.uid]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid} />
+      </div>
+    </div>
+  );
 }
