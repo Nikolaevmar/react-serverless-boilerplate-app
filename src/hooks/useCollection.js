908bf16cf85cfb98955cef68984a242cb72e2d2d
Nikolaevmar <marioniko001@gmail.com>
2021-11-28 19:06:07 +0200

    ordered transactions by descending order

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,19 +1,23 @@
 import { useEffect, useState,useRef } from "react";
 import { firestore } from "../firebase/config";
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _order) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   //if we don't use a ref we will get an infinite loop in useEffect
  //_query is an array and is 'different' on every function call which triggers the infinite loop
   const query = useRef(_query).current;
+  const order = useRef(_order).current;
 
   useEffect(() => {
     let ref = firestore.collection(collection);
 
     if(query){
       ref = ref.where(...query)
+    }
+    if(order){
+      ref = ref.orderBy(...order)
     }
 
     //the snapshot represents the collection at that moment in time & will contain the docs on it
@@ -27,11 +31,11 @@ export const useCollection = (collection, _query) => {
         setError(null);
       },
       (error) => {
-        setError(error);
+        setError('Could not fetch the data');
       }
     );
     //unsub on unmount
     return () => unsub();
-  }, [collection, query]);
+  }, [collection, query, order]);
   return { documents, error };
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,7 @@ import { useCollection } from "../../hooks/useCollection";
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("transactions", ['uid', '==', user.uid]);
+  const { documents, error } = useCollection("transactions", ['uid', '==', user.uid], ["createAt", "desc"]);
 
   return (
     <div className={styles.container}>
